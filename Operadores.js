e81ec19b57f80c67ba693046dc8f4a394e656266
Isabel de la Cuadra <isabel.de.la.cuadra@gmail.com>
2023-01-29 15:18:11 -0300

    Operadores y precedencia

---
+++ Operadores.js
@@ -0,0 +1,29 @@
+console.log(3 + 5);
+console.log(5 - 3);
+console.log(3 * 5);
+console.log(15 / 5);
+console.log(16 % 5);
+console.log(3 + 5 * 2 - 8);
+console.log((3 + 5) * 2 - 8);
+console.log("Hola " + "Isabel");
+var nombre = "Isabel";
+var saludo = "Hola " + nombre;
+console.log(saludo);
+
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
