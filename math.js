bfc53e8be4a5706d875fc2650b4f7b91cccaed0f
Isabel de la Cuadra <isabel.de.la.cuadra@gmail.com>
2023-01-29 17:37:06 -0300

    Math

---
+++ math.js
@@ -0,0 +1,8 @@
+console.log(Math.round(3.27));
+console.log(Math.floor(3.27));
+console.log(Math.ceil(3.27));
+
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+
+console.log(Math.random());
