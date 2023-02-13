01867a979d3fb8dbba8774651aad5d1eef4a4384
Isabel de la Cuadra <isabel.de.la.cuadra@gmail.com>
2023-02-12 22:59:47 -0300

    Fin Objects

--- objects.js
+++ objects.js
@@ -43,3 +43,20 @@ var libro = { autor: 'Borges', genero: 'Policial', año: 1990 };
 var todasLasPropiedades = Object.keys(libro);
 
 console.log(todasLasPropiedades);
+
+var mundo = { continentes: 7, paises: 195, oceanos: 5 };
+for (let prop in mundo) {
+    console.log('Esta es la propiedad: ', prop);
+    console.log('Este es el valor: ', mundo[prop]);
+}
+
+//THIS
+var mascota = {
+    animal: 'Perro',
+    raza: 'Ovejero Alemán',
+    amistoso: true,
+    dueño: 'María López',
+    info: () => {
+        console.log('Mi perro es un  ' + this.raza);
+    },
+};
